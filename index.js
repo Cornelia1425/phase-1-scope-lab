718619eabbf69b816180a5c34c060f24fa1cbcd8
yiranshu <shuyiran1425@hotmail.com>
2024-03-20 13:43:34 -0400

    scope

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+/* function upperCaseCustomerName(name){
+    return customerName = name.toUpperCase()
+} */
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+  } 
+/* function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+} */
+//upperCaseCustomerName()
+//console.log('this is new:', customerName)
+
+
+
+/* function setBestCustomer(){
+    var bestCustomer = 'not bob'
+    return bestCustomer
+}
+setBestCustomer() */
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'; // This will throw an error
+  }
